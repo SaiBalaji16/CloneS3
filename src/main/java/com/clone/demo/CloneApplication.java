b73228c014f869c4d662593fe799ff5caca9fc12
ksbalaji <ksbalaji@INLEN8520078037.corp.capgemini.com>
2023-02-20 15:57:41 +0530

    Hello

---
+++ src/main/java/com/clone/demo/CloneApplication.java
@@ -0,0 +1,14 @@
+package com.clone.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloneApplication.class, args);
+		System.out.println("Heloo World");
+	}
+
+}
